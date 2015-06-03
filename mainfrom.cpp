c73102024c5352ccebc6c33a42c3c5d9a246a166
AwakeHateYou <eterente94@gmail.com>
2015-06-03 19:04:30 +0300

    work for now

--- mainfrom.cpp
+++ mainfrom.cpp
@@ -5,7 +5,7 @@
 #include <string.h>
 
 
-#define PI 3.14159265358979323846
+#define PI 3.141592
 #define N 100
 #define delta 0.1
 using namespace std;
@@ -19,14 +19,7 @@ void initU(double *mass, double dx){
 	for (int i = 0; i < N; i++)
 		mass[i + N*j] = ((S(dx*i, dx*j) - 2.0 * delta*delta )/ (delta*delta*delta*delta))
 		*exp(-S(dx*i, dx*j) / 2.0*delta*delta);
-		//mass[i + N*j] = exp(-S(dx*i, dx*j) / (2 * pow(delta, 2)));
-//	for (int j = 0; j < N; j++)
-//	for (int i = 0; i < N; i++){
-//		if (i == 0)
-//			mass[i + j*N] = mass[N - 1 + j*N];
-//		if (j == 0)
-//			mass[i + j*N] = mass[i + (N - 1)*N];
-//	}
+
 
 
 }
@@ -44,7 +37,7 @@ void print(double *mass, double dx){
 void printGNU(double *mass, double dx, char* filename){
 	ofstream fout;
 	fout.open(filename);
-	fout.precision(3);
+	//fout.precision(3);
 	for (int j = 0; j < N; j++){
 		for (int i = 0; i < N; i++)
 			fout << dx*i << ' ' << dx*j << ' ' << mass[i + j*N] << endl;
@@ -53,48 +46,49 @@ void printGNU(double *mass, double dx, char* filename){
 	fout.close();
 }
 
-//fftw_complex VVV(double m, double n, fftw_complex* R) {
-//	return (((-1/4*N*N)*R(m,n))/(math.sin(PI*m/N)*math.sin(PI*m/N) + math.sin(PI*n/N)*math.sin(PI*n/N)));
-//}
+double fillcomplex(double element, double m, double n, double dx){
+	return ((((-1.0)/(4.0*N*N)) * element)/(sin(PI*m*dx/N)*sin(PI*m*dx/N) + sin(PI*n*dx/N)*sin(PI*n*dx/N)));
+	//return 2*element;
+}
+double sinmagic(double i, double j){
+	return (sin(PI*i/N)*sin(PI*i/N) + sin(PI*j/N)*sin(PI*j/N));
+}
 
 int main(){
 	double CN = N*((N / 2) + 1);
 	double dx = 1.0/N;
-	fftw_complex *out, *Uc;
+	fftw_complex *out, *ncomplex;
 	double *U;
 	U = new double[N*N];
 	initU(U, dx);
-	printGNU(U, dx, (char*)"before.txt");
+	printGNU(U, dx, (char*)"first");
 	fftw_plan p, b;
-	Uc = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*CN);
+	ncomplex = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*CN);
 	out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex)*CN);
 	p = fftw_plan_dft_r2c_2d(N, N, U, out, FFTW_ESTIMATE);
 	fftw_execute(p);
 
-	double *D, *B;
-
+	double *out_double, *ncomplex_double;
+	//for(int k = 0; k < N; k++){
 	for(int i = 0; i < N ; i++)
-		for(int j = 0; j < N/2 + 1; j++) {
-			D = (double*)(&(out[i + j*N]));
-			B = (double*)(&(Uc[i + j*N]));
-			//cout << D[0] <<" "<< D[1]<< endl;
-		    B[0] = (((-1.0/(4.0*N*N))*D[0])/(sin(PI*i*dx/N)*sin(PI*i*dx/N) + sin(PI*j*dx/N)*sin(PI*j*dx/N)));
-			B[1] = (((-1.0/(4.0*N*N))*D[1])/(sin(PI*i*dx/N)*sin(PI*i*dx/N) + sin(PI*j*dx/N)*sin(PI*j*dx/N)));
+		for(int j = 0; j < N/2+1; j++) {
+			out_double = (double*)(&(out[i + j*N]));
+			ncomplex_double = (double*)(&(ncomplex[i + j*N]));
+			//out_double[0] = fillcomplex(out_double[0], j, i, dx);
+			//out_double[1] = fillcomplex(out_double[1], j, i, dx);
+			//cout << sinmagic(i, j)<<endl;
+			//cout << out_double[0] <<" "<< out_double[1]<< endl;
+		    	ncomplex_double[0] = (((-1.0/(4.0*N*N))*out_double[0])/sinmagic(i*dx, j*dx));
+			ncomplex_double[1] = (((-1.0/(4.0*N*N))*out_double[1])/sinmagic(i*dx, j*dx));
 		}
 
-//	for(int i = 0; i < N/2 + 1; i++)
-//		for(int j = 0; j < N; j++) {
-//			B = (double*)(&(Uc[i + j*N]));
-//			cout << B[0] << " " << B[1] << endl;
-//		}
-
-	b = fftw_plan_dft_c2r_2d(N, N, Uc, U, FFTW_ESTIMATE);
+	b = fftw_plan_dft_c2r_2d(1, N, ncomplex, U, FFTW_ESTIMATE);
 	fftw_execute(b);
 	fftw_destroy_plan(p);
 	fftw_destroy_plan(b);
 	fftw_free(out);
-	fftw_free(Uc);
-	printGNU(U, dx, (char*)"after.txt");
+	fftw_free(ncomplex);
+	printGNU(U, dx, (char*)"second");
 	delete[]U;
 	return 0;
 
